fd7a940395b9bd2a1cb82fdf270ccb6218bbdea4
ritikarawat01 <rtkrawat0@gmail.com>
2026-04-02 14:43:15 +0530

    Time: 1 ms (62.91%), Space: 42.4 MB (64.43%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                res = res | (1 << (31 - i));
+            }
+        }
+
+        return res;
+    }
+}
