93a557bab4dca72f1a150a68fba5a7470839c365
ritikarawat01 <rtkrawat0@gmail.com>
2026-09-22 16:46:41 +0530

    Time: 0 ms (100%), Space: 47.2 MB (30.06%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int tar) {
+        if(root == null){
+            return new TreeNode(tar);
+        }
+        if(  tar < root.val){
+            root.left = insertIntoBST(root.left, tar);
+        }
+        else{
+            root.right = insertIntoBST(root.right, tar);
+        }
+
+        return root;
+        
+    }
+}
